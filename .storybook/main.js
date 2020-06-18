ae8364e71fb72c87ef01a13affa9a7d78e78ae96
aytee17 <andrew.tang@outlook.com>
2020-06-19 01:00:37 +1000

    Add packages and configuration

---
+++ .storybook/main.js
@@ -0,0 +1,25 @@
+module.exports = {
+  stories: ['../src/**/*.stories.tsx'],
+  addons: [
+    "@storybook/preset-typescript",
+    '@storybook/addon-a11y/register',
+    '@storybook/addon-docs',
+    '@storybook/addon-knobs/register',
+    '@storybook/addon-viewport/register',
+  ],
+  webpackFinal: async config => {
+    config.module.rules.push({
+      test: /\.(ts|tsx)$/,
+      use: [
+        {
+          loader: require.resolve('ts-loader'),
+        },
+        {
+          loader: require.resolve('react-docgen-typescript-loader'),
+        },
+      ],
+    });
+    config.resolve.extensions.push('.ts', '.tsx')
+    return config;
+  }
+}
